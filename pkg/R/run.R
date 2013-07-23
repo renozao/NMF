1ff12680b6214c7a8c061963dfed31af6ef4c999
Renaud <renaud@tx.technion.ac.il>
2013-07-23 14:55:24 +0300

    Added file run.R to generate NMF reports

---
+++ pkg/R/run.R
@@ -0,0 +1,36 @@
+# Functions produce reports   
+# 
+# Author: Renaud Gaujoux
+# Created: 23 Jul 2013
+###############################################################################
+
+
+#' Run NMF Methods and Generate a Report
+#' 
+#' @param x target matrix
+#' @param rank factorization rank
+#' @param method list of methods to apply
+#' @param colClass reference class to assess accuracy
+#' @param ... extra paramters passed to \code{\link{nmf}}
+#' @param output output HTML file
+#' @param template template Rmd file
+#' 
+nmfReport <- function(x, rank, method, colClass = NULL, rowClass = NULL, ..., output = NULL, template = NULL){
+	
+	library(knitr)
+	if( is.null(template) )
+		template <- system.file('scripts/report.Rmd', package = 'NMF')
+	x <- force(x)
+	rank <- force(rank)
+	method <- force(method)
+	args <- list(...)
+	nmfRun <- function(x, rank, method, ...){
+		args <- expand_dots(args)
+		str(args)
+		do.call(nmf, c(list(x, rank, method), args))
+	}
+	knit2html(template)
+	res <- list(fits = res, accuracy = accuracy)
+	saveRDS(res, file = 'report_results.rds')
+	invisible(res)
+}
