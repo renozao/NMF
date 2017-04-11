f4706767876ac9e29fb61dbc2581144d961b7fdf
Renaud <renaud@tx.technion.ac.il>
2017-04-11 16:09:51 +0200

    Moved gfile to pkgmaker

--- R/aheatmap.R
+++ R/aheatmap.R
@@ -1012,40 +1012,6 @@ vplayout <- function(x, y = NULL, ..., verbose = getOption('verbose')){
 	}	
 #})
 
-#' Open a File Graphic Device
-#'
-#'  Opens a graphic device depending on the file extension
-#' 
-#' @keywords internal
-gfile <- function(filename, width, height, ...){ 
-	# Get file type
-	r = regexpr("\\.[a-zA-Z]*$", filename)
-	if(r == -1) stop("Improper filename")
-	ending = substr(filename, r + 1, r + attr(r, "match.length"))
-	
-	f = switch(ending,
-			pdf = function(x, ...) pdf(x, ...),
-			svg = function(x, ...) svg(x, ...),			
-			png = function(x, ...) png(x, ...),
-			jpeg = function(x, ...) jpeg(x, ...),
-			jpg = function(x, ...) jpeg(x, ...),
-			tiff = function(x, ...) tiff(x, compression = "lzw", ...),
-			bmp = function(x, ...) bmp(x, ...),
-			stop("File type should be: pdf, svg, png, bmp, jpg, tiff")
-	)
-	
-	args <- c(list(filename), list(...))	
-	if( !missing(width) ){
-		args$width <- as.numeric(width)
-		args$height <- as.numeric(height)
-		if( !ending %in% c('pdf','svg') && is.null(args[['res']]) ){
-			args$units <- "in"
-			args$res <- 300
-		}
-	}
-	do.call('f', args)	
-}
-
 #gt <- function(){
 #	
 #	x <- rmatrix(20, 10)
